c850b0b8ca6bf474886802f37319d10fd869118a
Tomas Cuevas <memo_mister398@hotmail.com>
2022-10-28 14:10:20 -0300

    fix(Root): add image domain in next.config for load images correctly

--- next.config.js
+++ next.config.js
@@ -3,7 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["localhost:3000", "localhost", "res.cloudinary.com"],
+    domains: [
+      "localhost:3000",
+      "localhost",
+      "res.cloudinary.com",
+      "https://teslo-test-tc.vercel.app",
+      "teslo-test-tc.vercel.app",
+    ],
   },
 };
 
