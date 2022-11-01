6dc4ffe62d9764eccb32acab4c8aa1a75eff4845
Tomas Cuevas <memo_mister398@hotmail.com>
2022-11-01 17:21:45 -0300

    refactor(Root): change loader style

--- components/ui/FullScreenLoading.tsx
+++ components/ui/FullScreenLoading.tsx
@@ -2,7 +2,7 @@ import { CircularProgress } from "@mui/material";
 
 export const FullScreenLoading = () => {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="absolute top-0 left-0 flex h-screen w-screen items-center justify-center">
       <CircularProgress />
     </div>
   );
